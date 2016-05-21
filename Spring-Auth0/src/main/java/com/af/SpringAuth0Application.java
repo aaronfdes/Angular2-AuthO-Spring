57bbc621022d69aa0e346752475c1e3d5d0bde39
Aaron Fernandes <aaronfernandes1406@gmail.com>
2016-05-21 19:39:32 +0530

    added spring boot project

---
+++ Spring-Auth0/src/main/java/com/af/SpringAuth0Application.java
@@ -0,0 +1,12 @@
+package com.af;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuth0Application.class, args);
+	}
+}
---
+++ Spring-Auth0/src/test/java/com/af/SpringAuth0ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.af;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringAuth0Application.class)
+public class SpringAuth0ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
